4cfb3bb21f070e11d431338616c661df4268c11b
Furio Alessio <alefurio@hotmail.it>
2020-11-06 14:54:13 +0100

    bonus immagine

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var app = new Vue({
     data: {
         message: 'Hello World!!!',
         classeH1: 'red',
+        imgsample: '<img src="https://files.brumbrum.it/blog/wp-content/uploads/2020/07/24150420/Muscle-car-caratteristiche-e-i-modelli-pi%C3%B9-famosi.jpg">'
     },
     methods: {
         cambioClasse(){
@@ -15,6 +16,8 @@ var app = new Vue({
             else {
                 this.classeH1 = 'red';
             }
-        }
+        },
+
     }
+
 });
